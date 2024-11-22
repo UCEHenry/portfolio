bf696a4a2baa8a7c544d1ae2b2901af300c4d72f
Henry <uce.henrys@gmail.com>
2024-11-22 00:21:38 +0000

    Added socials, should fix links and consider adding more

---
+++ src/data/socials.ts
@@ -0,0 +1,15 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/ucehenrys",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/",
+    icon: IconLinkedin,
+  },
+];
