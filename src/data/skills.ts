4390566b272436f16408082a497a0bf502a58502
Henry <uce.henrys@gmail.com>
2024-11-30 06:21:00 +0000

    added icons for technical skills on about me. Fixed some tailwind css

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,13 +1,58 @@
+import IconJavaScript from "../components/icons/javascript.astro";
+import IconPython from "../components/icons/python.astro";
+import IconJava from "../components/icons/java.astro";
+import IconNodeJS from "../components/icons/nodejs.astro";
+import IconReact from "../components/icons/react.astro";
+import IconAstro from "../components/icons/astro.astro";
+import IconHTML from "../components/icons/html.astro"
+import IconCSS from "../components/icons/css.astro"
+import IconTailwind from "../components/icons/tailwind.astro"
+import IconSQL from "../components/icons/sql.astro"
+import IconMongoDB from "../components/icons/mongo.astro"
+
 export const SKILLS = [
-    "JAVA",
-    "Python",
-    "JavaScript",
-    "React",
-    "Espress",
-    "Astro",
-    "HTML",
-    "CSS",
-    "SQL",
-    "Mongo",
-    "NodeJS"
-]
+    {
+        name: "JavaScript",
+        icon: IconJavaScript,
+    },
+    {
+        name: "JAVA",
+        icon: IconJava,
+    },
+    {
+        name: "Python",
+        icon: IconPython,
+    },
+    {
+        name: "NodeJS",
+        icon: IconNodeJS,
+    },
+    {
+        name: "React",
+        icon: IconReact,
+    },
+    {
+        name: "Astro",
+        icon: IconAstro,
+    },
+    {
+        name: "HTML",
+        icon: IconHTML,
+    },
+    {
+        name: "CSS",
+        icon: IconCSS,
+    },
+    {
+        name: "Tailwind",
+        icon: IconTailwind,
+    },
+    {
+        name: "SQL",
+        icon: IconSQL,
+    },
+    {
+        name: "MongoDB",
+        icon: IconMongoDB,
+    },
+];
